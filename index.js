3df7698f82976d34aaab62111773be5ad544ff85
Liang Xingchen <liang@maichong.it>
2016-03-15 13:42:25 +0800

    bcrypt

--- index.js
+++ index.js
@@ -6,9 +6,7 @@
 
 'use strict';
 
-const alaska = require('alaska');
-const _ = require('lodash');
-const md5 = require('md5');
+const bcrypt = require('bcrypt');
 
 exports.views = {
   cell: {
@@ -23,6 +21,7 @@ exports.views = {
 
 exports.plain = String;
 
+
 /**
  * 初始化Schema
  * @param field   alaksa.Model中的字段配置
@@ -30,8 +29,52 @@ exports.plain = String;
  * @param Model
  */
 exports.initSchema = function (field, schema, Model) {
-  schema.path(field.path, _.defaults({
+  field.workFactor = field.workFactor || 10;
+  let NEED_HASHING = '__' + field.path + '_needs_hassing';
+  schema.path(field.path, {
     type: String,
-    set: md5
-  }, field.options));
+    set: function (password) {
+      this[NEED_HASHING] = true;
+      return password;
+    }
+  });
+  Model.underscoreMethod(field.path, 'data', function () {
+    return this.get(field.path) ? '' : null;
+  });
+
+  /**
+   * 比较密码
+   */
+  Model.underscoreMethod(field.path, 'compare', function (candidate) {
+    let record = this;
+    return new Promise(function (resolve, reject) {
+      let value = record.get(field.path);
+      if (!value) return resolve(false);
+      bcrypt.compare(candidate, record.get(field.path), function (error, res) {
+        if (error) return reject(error);
+        resolve(res);
+      });
+    });
+  });
+
+  schema.pre('save', function (next) {
+    let record = this;
+    if (!record.isModified(field.path) || !record[NEED_HASHING]) {
+      return next();
+    }
+
+    if (!record.get(field.path)) {
+      record.set(field.path, undefined);
+      return next();
+    }
+
+    bcrypt.genSalt(field.workFactor, function (err, salt) {
+      if (err) return next(err);
+      bcrypt.hash(record.get(field.path), salt, function (error, hash) {
+        if (error) return next(err);
+        record.set(field.path, hash);
+        next();
+      });
+    });
+  });
 };
