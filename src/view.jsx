6afca8671ae40f58ae70093ce75269d936dced66
Liang Xingchen <liang@maichong.it>
2016-04-22 01:44:16 +0800

    0.8.1

--- src/view.jsx
+++ src/view.jsx
@@ -108,7 +108,7 @@ export default class PasswordFieldView extends React.Component {
 
     let label = field.nolabel ? '' : field.label;
 
-    if (field.fullWidth) {
+    if (field.horizontal === false) {
       let labelElement = label ? (
         <label className="control-label">{label}</label>
       ) : null;
